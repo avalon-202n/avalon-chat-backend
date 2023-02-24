fe569e159290eae2cc0d8da4216646bcad073ef0
ndy2 <emrdbs12@gmail.com>
2023-02-24 13:16:39 +0900

    add and apply `checkstyle`
    use naver convention (https://github.com/naver/hackday-conventions-java)

--- src/main/java/com/avalon/avalonchat/AvalonChatBackendApplication.java
+++ src/main/java/com/avalon/avalonchat/AvalonChatBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AvalonChatBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AvalonChatBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AvalonChatBackendApplication.class, args);
+	}
 
 }
