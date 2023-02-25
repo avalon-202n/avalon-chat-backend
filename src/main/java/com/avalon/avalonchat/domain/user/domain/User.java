5cbfa809fae769203afa172520a8513215b8eb31
YOONSUNGHYEON <qead753951@naver.com>
2023-02-25 14:56:15 +0900

    add user domain, service, controller

---
+++ src/main/java/com/avalon/avalonchat/domain/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.avalon.avalonchat.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/avalon/avalonchat/domain/user/service/UserService.java
@@ -0,0 +1,4 @@
+package com.avalon.avalonchat.domain.user.service;
+
+public interface UserService {
+}
