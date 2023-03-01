4cef8699448d1319fa9a82b778a36d6565007c8f
ndy2 <emrdbs12@gmail.com>
2023-03-01 20:48:48 +0900

    feat: add auditing configuration

--- src/main/java/com/avalon/avalonchat/domain/model/BaseDateTimeEntity.java
+++ src/main/java/com/avalon/avalonchat/domain/model/BaseDateTimeEntity.java
@@ -3,16 +3,22 @@ package com.avalon.avalonchat.domain.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
 
 /**
  * 객체의 공통 매핑 정보
  * 생성일, 수정일
  */
+@Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseDateTimeEntity {
 	@CreatedDate
 	@Column(name = "created_at", updatable = false, nullable = false)
