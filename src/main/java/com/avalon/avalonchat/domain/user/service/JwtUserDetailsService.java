905a57e57341e24b29a360ea3a24bef13cdffcf8
YOONSUNGHYEON <qead753951@naver.com>
2023-03-12 20:22:01 +0900

    add : JwtUserDetailsService 생성

---
+++ src/main/java/com/avalon/avalonchat/domain/user/service/JwtUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.avalon.avalonchat.domain.user.service;
+
+import com.avalon.avalonchat.domain.user.domain.Email;
+import com.avalon.avalonchat.domain.user.dto.SecurityUser;
+import com.avalon.avalonchat.domain.user.domain.User;
+import com.avalon.avalonchat.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class JwtUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public SecurityUser loadUserByUsername(String email) throws UsernameNotFoundException {
+		User findUser = userRepository.findByEmail(Email.of(email))
+			.orElseThrow(() -> new UsernameNotFoundException("회원 정보를 찾을 수 없었습니다."));
+		GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+		return new SecurityUser(findUser.getId(), findUser.getEmail().getValue(), findUser.getPassword(), Collections.singleton(grantedAuthority));
+	}
+}
