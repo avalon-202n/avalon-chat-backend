bdb0b5dc8112f9dbde809d53c5db5dfb7597105f
YOONSUNGHYEON <qead753951@naver.com>
2023-03-12 19:55:38 +0900

    add : AuthenticationEntryPoint 추가

---
+++ src/main/java/com/avalon/avalonchat/global/configuration/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.avalon.avalonchat.global.configuration.security;
+
+import com.avalon.avalonchat.global.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+		response.setStatus(HttpStatus.FORBIDDEN.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		try (OutputStream os = response.getOutputStream()){
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.writeValue(os, new ErrorResponse(HttpStatus.FORBIDDEN, authException));
+			os.flush();
+		}
+	}
+}
