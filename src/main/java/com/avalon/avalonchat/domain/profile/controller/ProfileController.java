e1f1d3d97d8b9a5fae7457157336be2a3f7a2254
hgene0929 <hgene0929@gmail.com>
2023-03-14 14:54:44 +0900

    fix: 프로필 등록 컨트롤러 수정
    - prefix 수정: 공통 v1 제거
    - userId 추출로직 수정: header에서 토큰 가져와서 userId 꺼내는 방식 -> 시큐리티 인증객체를 어노테이션 가지고 주입받아서 userId 바로 꺼내는 방식으로 변경할 예정(아직 소스 코드 풀 받기 전이라 TODO에 내용 변경해두었습니다)
    - 요청 바디 바인딩 어노테이션 수정: multipart/form-data 바인딩 어노테이션 @ModelAttribute로 변경
    
    ** 추가로 컨트롤러 테스트 코드도 바로 짜서 커밋하도록 하겠습니다.

--- src/main/java/com/avalon/avalonchat/domain/profile/controller/ProfileController.java
+++ src/main/java/com/avalon/avalonchat/domain/profile/controller/ProfileController.java
@@ -7,30 +7,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/v1/profiles")
+@RequestMapping("/profiles")
 public class ProfileController {
 
 	private final ProfileService profileService;
-	private final String HEADER_AUTHORIZATION = "AUTHORIZATION";
-	private final String HEADER_AUTHORIZATION_PREFIX = "Bearer ";
 
 	@PostMapping()
 	public ResponseEntity<ProfileResponse> addProfile(
-		HttpServletRequest httpRequest,
-		@RequestBody ProfileRequest request
+		ProfileRequest request
 	) {
-		String accessToken = httpRequest.getHeader(HEADER_AUTHORIZATION).replace(HEADER_AUTHORIZATION_PREFIX, "");
-
-		//TODO jwt 토큰에서 user_id 꺼내기
+		//TODO: @AuthenticationPrincipal 활용 -> SecurityUser 타입 Authentication 객체에서 userId 가져올 것
 		long id = 1L;
 
 		return profileService.addProfile(id, request);
