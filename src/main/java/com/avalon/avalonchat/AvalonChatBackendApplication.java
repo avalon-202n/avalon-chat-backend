cc85f7d80dc94b4b5db86776b2fce8f45801e35d
YOONSUNGHYEON <qead753951@naver.com>
2023-03-21 18:58:08 +0900

    refactor : 필요없는 코드 삭제

--- src/main/java/com/avalon/avalonchat/AvalonChatBackendApplication.java
+++ src/main/java/com/avalon/avalonchat/AvalonChatBackendApplication.java
@@ -2,12 +2,10 @@ package com.avalon.avalonchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class AvalonChatBackendApplication {
 
 	public static void main(String[] args) {
