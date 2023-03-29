c98ff2f651e4ee4e224f3c4c05478eb89d68ef3d
hgene0929@gmail.com <hgene0929@gmail.com>
2023-03-29 21:40:04 +0900

    chore: gcp 관련 코드 삭제

--- src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
+++ src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
@@ -2,9 +2,7 @@ package com.avalon.avalonchat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest
 class AvalonChatBackendApplicationTests {
 
