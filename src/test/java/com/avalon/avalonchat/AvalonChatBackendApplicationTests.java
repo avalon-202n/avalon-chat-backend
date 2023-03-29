03b9886d945f630b594ca752b3fc97fc65e26999
hgene0929@gmail.com <hgene0929@gmail.com>
2023-03-29 18:50:22 +0900

    chore: test properties 적용

--- src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
+++ src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
@@ -2,7 +2,9 @@ package com.avalon.avalonchat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest
 class AvalonChatBackendApplicationTests {
 
