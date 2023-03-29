68b6812e85317ae1f82a3230986d635e0414e2cc
hgene0929@gmail.com <hgene0929@gmail.com>
2023-03-29 10:36:06 +0900

    chore: reformat

--- src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
+++ src/test/java/com/avalon/avalonchat/AvalonChatBackendApplicationTests.java
@@ -1,6 +1,5 @@
 package com.avalon.avalonchat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AvalonChatBackendApplicationTests {
 
 	@Test
-	@Disabled
 	void contextLoads() {
 	}
 
