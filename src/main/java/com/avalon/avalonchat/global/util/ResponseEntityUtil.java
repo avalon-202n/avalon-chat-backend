656fa278ff5ea477d59d495e92a4784867654ed8
ndy2 <emrdbs12@gmail.com>
2023-04-02 10:53:23 +0900

    refactor(profile): apply util method created

--- src/main/java/com/avalon/avalonchat/global/util/ResponseEntityUtil.java
+++ src/main/java/com/avalon/avalonchat/global/util/ResponseEntityUtil.java
@@ -1,6 +1,7 @@
 package com.avalon.avalonchat.global.util;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.ResponseEntity;
 
 import lombok.NoArgsConstructor;
@@ -8,7 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public final class ResponseEntityUtil {
 
+	public static <T> ResponseEntity<T> created(T responseBody) {
+		return new ResponseEntity<>(responseBody, CREATED);
+	}
+
 	public static ResponseEntity<Void> noContent() {
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(NO_CONTENT);
 	}
 }
