1ab9ca289d30ec279d14497db9fd530ae2d5ec6b
ndy2 <emrdbs12@gmail.com>
2023-04-02 10:50:23 +0900

    refactor(profile): simplify ProfileAddRequest
    - it is way too complicated String is enough for now

--- src/main/java/com/avalon/avalonchat/domain/profile/dto/ProfileAddRequest.java
+++ src/main/java/com/avalon/avalonchat/domain/profile/dto/ProfileAddRequest.java
@@ -2,9 +2,6 @@ package com.avalon.avalonchat.domain.profile.dto;
 
 import java.time.LocalDate;
 
-import com.avalon.avalonchat.domain.profile.domain.Profile;
-import com.avalon.avalonchat.domain.user.domain.User;
-
 import lombok.Getter;
 
 @Getter
@@ -12,31 +9,20 @@ public class ProfileAddRequest {
 	private final LocalDate birthDate;
 	private final String nickname;
 	private final String bio;
-	private ProfileImageAddRequest profileImage;
-	private BackgroundImageAddRequest backgroundImage;
+	private final String profileImage;
+	private final String backgroundImage;
 
 	public ProfileAddRequest(
 		LocalDate birthDate,
 		String nickname,
 		String bio,
 		String profileImage,
-		String backgroundImage) {
+		String backgroundImage
+	) {
 		this.birthDate = birthDate;
 		this.nickname = nickname;
 		this.bio = bio;
-		setProfileImage(profileImage);
-		setBackgroundImage(backgroundImage);
-	}
-
-	public void setProfileImage(String profileImageUrl) {
-		this.profileImage = new ProfileImageAddRequest(profileImageUrl);
-	}
-
-	public void setBackgroundImage(String backgroundImageUrl) {
-		this.backgroundImage = new BackgroundImageAddRequest(backgroundImageUrl);
-	}
-
-	public Profile toEntity(User user) {
-		return new Profile(user, this.bio, this.birthDate, this.nickname);
+		this.profileImage = profileImage;
+		this.backgroundImage = backgroundImage;
 	}
 }
