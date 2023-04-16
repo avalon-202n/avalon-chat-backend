208546fe8cda716cf1fb79ca015f09050df84096
YOONSUNGHYEON <qead753951@naver.com>
2023-04-16 16:17:56 +0900

    add : 아이디 찾기 & 비밀번호 찾기 더미 controller 생성

--- src/main/java/com/avalon/avalonchat/domain/login/controller/LoginController.java
+++ src/main/java/com/avalon/avalonchat/domain/login/controller/LoginController.java
@@ -2,9 +2,8 @@ package com.avalon.avalonchat.domain.login.controller;
 
 import static com.avalon.avalonchat.global.error.ErrorResponseWithMessages.*;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
 import com.avalon.avalonchat.domain.login.dto.LoginRequest;
 import com.avalon.avalonchat.domain.login.dto.LoginResponse;
@@ -15,6 +14,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Tag(name = "login", description = "로그인과 관련된 endpoint 모음")
+@RequestMapping("/login")
 @RestController
 public class LoginController {
 
@@ -22,8 +23,26 @@ public class LoginController {
 
 	@Operation(summary = "로그인")
 	@ErrorResponseApi(messages = INVALID_LENGTH, args = {"Password", "max", "min"})
-	@PostMapping("/login")
+	@PostMapping
 	public LoginResponse login(
+		@RequestBody LoginRequest request
+	) {
+		return loginService.login(request);
+	}
+
+	@Operation(summary = "비밀번호 찾기")
+	@ErrorResponseApi(messages = INVALID_LENGTH, args = {"Password", "max", "min"})
+	@GetMapping("/password/find")
+	public LoginResponse passwordFind(
+		@RequestBody LoginRequest request
+	) {
+		return loginService.login(request);
+	}
+
+	@Operation(summary = "아이디 찾기")
+	@ErrorResponseApi(messages = INVALID_LENGTH, args = {"Password", "max", "min"})
+	@GetMapping("/email/find")
+	public LoginResponse EmailFind(
 		@RequestBody LoginRequest request
 	) {
 		return loginService.login(request);
