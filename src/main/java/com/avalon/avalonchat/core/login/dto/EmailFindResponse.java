64e64e143a874213df30a97fbab76954cf0218a2
ndy2 <emrdbs12@gmail.com>
2023-05-24 15:04:09 +0900

    chore: DTO, 메서드 시그니쳐 일관성

--- src/main/java/com/avalon/avalonchat/core/login/dto/EmailFindResponse.java
+++ src/main/java/com/avalon/avalonchat/core/login/dto/EmailFindResponse.java
@@ -4,14 +4,14 @@ import javax.validation.constraints.NotNull;
 
 import com.avalon.avalonchat.core.user.domain.Email;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailFindResponse {
 	@NotNull
-	@Schema(description = "이메일", example = "test1234@gmail.com")
 	private Email email;
 }
