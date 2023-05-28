39c1fe939dbd5a8d793e45c734aeef91d8808612
ndy2 <emrdbs12@gmail.com>
2023-05-28 19:36:22 +0900

    chore: 로그인 응답에 프로필 정보 추가

--- src/main/java/com/avalon/avalonchat/core/login/application/TokenService.java
+++ src/main/java/com/avalon/avalonchat/core/login/application/TokenService.java
@@ -2,12 +2,12 @@ package com.avalon.avalonchat.core.login.application;
 
 import java.util.Map;
 
-import com.avalon.avalonchat.core.user.domain.User;
+import com.avalon.avalonchat.core.profile.domain.Profile;
 
 public interface TokenService {
 	Map<String, Object> parseClaim(String token);
 
-	String createAccessToken(User user, long profileId);
+	String createAccessToken(Profile profile);
 
 	String createRefreshToken();
 
