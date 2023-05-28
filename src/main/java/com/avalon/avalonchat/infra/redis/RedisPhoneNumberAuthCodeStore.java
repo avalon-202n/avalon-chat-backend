a007449c66f24ebc3fd8d56d97186cee22587121
ndy2 <emrdbs12@gmail.com>
2023-05-28 20:55:22 +0900

    chore: RedisPhoneNumberAuthCodeStore 에 스크립트 적용

--- src/main/java/com/avalon/avalonchat/infra/redis/RedisPhoneNumberAuthCodeStore.java
+++ src/main/java/com/avalon/avalonchat/infra/redis/RedisPhoneNumberAuthCodeStore.java
@@ -1,34 +1,50 @@
 package com.avalon.avalonchat.infra.redis;
 
+import java.util.List;
+
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.RedisScript;
 
 import com.avalon.avalonchat.core.user.application.PhoneNumberAuthCodeStore;
 import com.avalon.avalonchat.core.user.application.keyvalue.AuthCodeValue;
 import com.avalon.avalonchat.core.user.application.keyvalue.PhoneNumberKey;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class RedisPhoneNumberAuthCodeStore implements PhoneNumberAuthCodeStore {
 
-	private final ValueOperations<PhoneNumberKey, AuthCodeValue> phoneNumberAuthOperations;
+	private final RedisTemplate<PhoneNumberKey, AuthCodeValue> redisTemplate;
 
-	public RedisPhoneNumberAuthCodeStore(
-		RedisTemplate<PhoneNumberKey, AuthCodeValue> phoneNumberAuthRedisTemplate
-	) {
-		this.phoneNumberAuthOperations = phoneNumberAuthRedisTemplate.opsForValue();
-	}
+	private final RedisScript<Boolean> isAuthenticatedScript = RedisScript.of(
+		"local value = redis.call('GET', KEYS[1]) "
+			+ " return value ~= nil and value == 'AUTH_CODE::[AUTHENTICATED]' ",
+		Boolean.class
+	);
+
+	private final RedisScript<Boolean> checkKeyValueMatchesScript = RedisScript.of(
+		"local value = redis.call('GET', KEYS[1]) "
+			+ " if value == ARGV[1] "
+			+ " then "
+			+ "  redis.call('SET', KEYS[1], 'AUTH_CODE::[AUTHENTICATED]') "
+			+ "  return true "
+			+ " end "
+			+ " return false",
+		Boolean.class
+	);
 
 	@Override
 	public void save(PhoneNumberKey key, AuthCodeValue value) {
-
+		redisTemplate.opsForValue().set(key, value);
 	}
 
 	@Override
 	public boolean isAuthenticated(PhoneNumberKey key) {
-		return false;
+		return redisTemplate.execute(isAuthenticatedScript, List.of(key));
 	}
 
 	@Override
 	public boolean checkKeyValueMatches(PhoneNumberKey key, AuthCodeValue value) {
-		return false;
+		return redisTemplate.execute(checkKeyValueMatchesScript, List.of(key), value);
 	}
 }
